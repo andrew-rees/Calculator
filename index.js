ec4cbacf979ec876ee25fd69800734974e3f104b
andrew-rees <38333904+andrew-rees@users.noreply.github.com>
2018-04-18 23:11:31 +0100

    Add files via upload
    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-console.log("Welcome to the Calculator");
+//Calculator Index File
+//Tasks are to: prompt a user to enter a number; 
+//Prompt to enter a second number;
+//Multiply and return the results
 
+
+const readline = require('readline-sync');
+
+//bit at top
+console.log("Welcome to the Calculator");
+console.log("_-_-_-_-_-_")
+
+
+//first input
+console.log("Please enter your first number:");
+//prompt for input, then define it as a constant
+const firstResponse = readline.prompt();
+//turn it into a number
+const firstInput = +firstResponse;
+
+//second input
+console.log("Thank you, please enter your second number");
+//prompt for input, then define it as a constant
+const secondResponse = readline.prompt();
+//turn it into a number
+const secondInput = +secondResponse;
+
+//output - calculate the answer
+const answer = (firstInput * secondInput);
+//display
+console.log("This is the answer " + answer);
